45c033d05cc3e05cc4a897f17af7b270eba18fa4
dmknapp2385 <dmknapp2385@gmail.com>
2024-09-19 20:15:52 -0700

    initial commit

---
+++ Book.java
@@ -0,0 +1,36 @@
+
+import java.util.Comparator;
+
+
+public class Book {
+
+    boolean read;
+    String author;
+    String title;
+    int rating;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void setToRead() {
+        if (!read) {
+            this.read = true;
+        }
+    }
+
+    /*
+     * @pre Rating is an integer between 1 and 5
+     * 
+     */
+    public void rate(int rating) {
+        this.rating = rating;
+    }
+
+    static class compareByTitle implements Comparator<Book>{
+        public int compare(Book bOne, Book bTwo){
+            return bOne.title.compareTo(bTwo.title);
+        }
+    }
+}
